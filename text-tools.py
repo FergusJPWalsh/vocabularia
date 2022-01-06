6a2be2c54274b6efcb2becc022f622fc4fa5b5b7
Fergus <fergusjpwalsh@gmail.com>
2022-01-06 23:05:33 +0000

    Cleaning

---
+++ text-tools.py
@@ -0,0 +1,29 @@
+import csv
+import re
+headwords = []
+entry_texts = []
+bad_entries = []
+with open ("bwl-fjpw.tsv", "r", encoding="utf-8") as f:
+    # lexicon = csv.reader(f, delimiter="\t")
+    # for entry in lexicon:
+        # headword = entry[0]
+        # entry_text = entry[1]
+        # headwords.append(headword)
+        # entry_texts.append(entry_text)
+# for i, headword in enumerate(headwords):
+    # if re.search("[^A-Z]", headword):
+        # print(i+1, headword)
+
+    lexicon = f.readlines()
+    for i, entry in enumerate(lexicon):
+        tabs = entry.count("\t")
+        if tabs != 2:
+            print(i+1)
+            bad_entries.append(entry)
+
+print("Len bad_entries:",len(bad_entries))
+# print("bad_entries:\n",bad_entries)
+
+all_text = " ".join(lexicon)
+letter_set = set(all_text)
+print(sorted(letter_set))
